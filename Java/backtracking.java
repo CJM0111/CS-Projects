f01d4d4faad1704c5c510eecc485896b1ca797da
Chris McDonald <cjmcdona@butler.edu>
2015-01-25 18:56:23 -0500

    Update backtracking.java

--- Java/backtracking.java
+++ Java/backtracking.java
@@ -2,45 +2,46 @@ import java.io.*;
 import java.util.Scanner;
 
 /**
-// this program calculates the optimal solution
-// for the 0-1 Knapsack problem using the backtracking algorithm
-@author Chris McDonald - The Program: Backtracking Algorithm
+ * This program calculates the optimal solution
+ * for the 0-1 Knapsack problem using the backtracking algorithm
+ * @author Chris McDonald - The Program: Backtracking Algorithm
 */
 
 public class backtracking
 {
-// sumPrice[n] holds the sum of all values from price[n] on up.
+   // sumPrice[n] holds the sum of all values from price[n] on up.
    static int [] sumPrice;
    
    public static void main(String [] args)
    throws IOException, FileNotFoundException
-   {  boolean [] max;
+   {  
+      boolean [] max;
       int [] weight, price;
       int n, maxWeight;
       String readIn = null;
       Scanner cin = new Scanner(System.in);
-	  readIn();
-	  readIn = cin.nextLine();
+      readIn();
+      readIn = cin.nextLine();
       Scanner input = new Scanner(new File(readIn));
       n = input.nextInt();
       maxWeight = input.nextInt();
       weight=new int[n];
       price =new int[n];
       max=new boolean[n];
-      for(int i=0; i<n; i++ )
-      {  price[i] = input.nextInt();
-         weight[i]  = input.nextInt();
-      }
+      for(int i=0; i<n; i++ ) { 
+      	   price[i] = input.nextInt();
+           weight[i]  = input.nextInt();
+      } // end for
       knapSack(maxWeight, max, weight, price);
       report(max, weight, price);
     }
     
- 	// This method will call the recursive backtracking method
- 	// The recursive method needs to receive and update information
+    // This method will call the recursive backtracking method
+    // The recursive method needs to receive and update information
     // on the current state and the optimal solution. This is
     // handled through passing arrays;
     // current[] will hold [0] current weight, [1] current price,
- 	// and [2] current maximum value;
+    // and [2] current maximum value;
     // max[] is the boolean vector corresponding to that
     // current maximum value;
     // used[] is the boolean vector used to generate the
@@ -48,18 +49,19 @@ public class backtracking
     
     public static void knapSack(int maxWt, boolean [] max, int [] weight, int [] price)
     {
-    	// places the first possible solution into max[]
+    	// Places the first possible solution into max[]
     	int [] current = { 0, 0, Integer.MIN_VALUE };
     	boolean [] used = new boolean[max.length];
     	int i;
-    	// calculates the bound for a particular node;
-    	// the bound is based on the sum of values from the current node
+    	// Calculates the bound for a particular node
+    	// The bound is based on the sum of values from the current node
     	// and all of the nodes on the path to the top
     	sumPrice = new int[price.length];
     	i = price.length - 1;
     	sumPrice[i] = price[i];
-    	while( --i >= 0 )
-    	{ sumPrice[i] = price[i] + sumPrice[i+1]; }
+    	while( --i >= 0 ) { 
+        	sumPrice[i] = price[i] + sumPrice[i+1]; 
+    	} // end while loop
     	knapSack(maxWt, used, 0, weight, price, max, current);
     }
     
@@ -70,81 +72,69 @@ public class backtracking
     // current[] - [0] current weight, 
     // [1] current price, and [2] current max value
     
-    static void knapSack(int maxW, boolean [] used, int item, int [] weight, int [] price,
-                        boolean [] max, int [] current)
+    static void knapSack(int maxW, boolean [] used, int item, int [] weight, int [] price, boolean [] max, int [] current)
     { 
-      // checks whether all positions have been considered
-      if(item == max.length)
-      {
-         if(current[1] > current[2])
-         { 
-         	if(current[2] < 0)
-         	{
-            	System.out.println("The first possible solution is: " + current[1]);
-            }
-            else
-            {
-   				System.out.println(current[1] + " replaces " + current[2]);
-   			}
-         // stores the value of the optimal node
-            current[2] = current[1];
-            System.arraycopy(used, 0, max, 0, used.length);
-         }
-         else
-         {
+      // Checks whether all positions have been considered
+      if(item == max.length) {
+         if(current[1] > current[2]) { 
+         	if(current[2] < 0) {
+            		System.out.println("The first possible solution is: " + current[1]);
+                } // end if
+         	else {
+   	 		System.out.println(current[1] + " replaces " + current[2]);
+   	 	}
+        	// Stores the value of the optimal node
+         	current[2] = current[1];
+         	System.arraycopy(used, 0, max, 0, used.length);
+      	 } // end if
+      	 else {
          	System.out.println(current[1] + " isn't valid.");
-         }
-      }
-      else
-      {
-      // checks if an item exceeds the weight limit on the knapsack
-         if(current[0] + weight[item] <= maxW)
-         {
-         // creates a bound based on the possible outcome from a promising node
-            if(current[1] + sumPrice[item] > current[2])
-            {
+      	 } // end else
+      } // end if
+      else {
+         // Checks if an item exceeds the weight limit on the knapsack
+         if(current[0] + weight[item] <= maxW) {
+            // Creates a bound based on the possible outcome from a promising node
+            if(current[1] + sumPrice[item] > current[2]) {
                used[item] = true;
                current[0] += weight[item];
                current[1] += price[item];
                knapSack(maxW,used,item+1,weight,price,max,current);
-            // resets the array to the values before the item was used
+            // Resets the array to the values before the item was used
                used[item] = false;
                current[0] -= weight[item];
                current[1] -= price[item];
-            }
-            else
-            {  
+            } // end if
+            else {  
             	System.out.println("The node at position " + item + " with a value of " + current[1] + " is nonpromising.");
-            	return;  // using the values above the current node will not work
-            }
-         }
+            	return;  // Using the values above the current node will not work
+            } // end else
+         } // end if
          knapSack(maxW, used, item+1, weight, price, max, current);
-      }
+      } // end else
    }
    
    static void report (boolean [] max, int [] weight, int [] price )
    {  
     	int totalW = 0, totalprice = 0;
-    	for (int i=0; i<max.length; i++ )
-        {
-        	if(max[i])
-        	{  
+    	for (int i=0; i<max.length; i++ ) {
+        	if(max[i]) {  
         		totalW += weight[i];
-            	totalprice  += price[i];
-         	}
-        }
+            		totalprice  += price[i];
+         	} // end if
+        } // end for loop
         result(totalprice, totalW);
     }
     
-	public static void readIn()
-	{
-		System.out.println("Enter the name of your file:");
-		System.out.println("(The format should be /Users/name/~)");
-	}
+    public static void readIn()
+    {
+	System.out.println("Enter the name of your file:");
+	System.out.println("(The format should be /Users/name/~)");
+    }
 	
-	public static void result(double totalprice, double totalW)
-	{
-		System.out.println("This is the optimal price: " + totalprice + " dollars.");
-		System.out.println("And this is the optimal weight: " + totalW + " pounds.");
-	}
+    public static void result(double totalprice, double totalW)
+    {
+	System.out.println("This is the optimal price: " + totalprice + " dollars.");
+	System.out.println("And this is the optimal weight: " + totalW + " pounds.");
+    }
 }
